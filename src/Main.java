0b0b284e36bafd15e44c09daa07e9ae05d923632
FeralHorse <pusiket90@yandex.ru>
2023-11-13 14:24:28 +0300

    более простой способ реализации

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkAuth("12341234123412341234","1","1");
     }
-
+    public static void checkAuth(String login, String password, String confirmPassword) {
+        if(login.length() > 20){
+            throw new RuntimeException("WrongLoginException");
+        }
+        if(isValidPasswordAndLogin(login)){
+            throw new RuntimeException("WrongLoginException");
+        }
+        if(password.length() > 20){
+            throw new RuntimeException("WrongPasswordException");
+        }
+        if(isValidPasswordAndLogin(password)){
+            throw new RuntimeException("WrongPasswordException");
+        }
+        if(!password.equals(confirmPassword)){
+            throw new RuntimeException("WrongPasswordException");
+        }
+    }
+    private static boolean isValidPasswordAndLogin(String password) {
+        return password.matches("\\w}");
+    }
 }
